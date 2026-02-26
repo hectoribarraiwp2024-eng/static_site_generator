2f4b08158c20d527cba4d2d514797437c3741a20
hectoribarraiwp2024-eng <hector.ibarra.iwp2024@gmail.com>
2026-02-26 00:23:03 -0600

    7: created methods to extract link and image urls and alt text from raw markdown text using regex

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
-from textnode import TextNode
+from textnode import TextNode, TextType
 from htmlnode import ParentNode, LeafNode
+from markdown_functions import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
 
 def main():
-    pass
+    pass    
 
 main()
