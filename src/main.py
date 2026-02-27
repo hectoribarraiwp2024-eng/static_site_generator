7b7548b47f2b148819f588b5482f8bdbbb18edce
hectoribarraiwp2024-eng <hector.ibarra.iwp2024@gmail.com>
2026-02-27 14:35:02 -0600

    8: created a text_to_textnodes method that takes inline markdown text and returns a list of textnodes with there texttype

--- src/main.py
+++ src/main.py
@@ -1,8 +1,16 @@
 from textnode import TextNode, TextType
 from htmlnode import ParentNode, LeafNode
-from markdown_functions import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
+from markdown_functions import markdown_to_blocks
 
 def main():
-    pass    
+    print(markdown_to_blocks('''
+    # This is a heading
+
+    This is a paragraph of text. It has some **bold** and _italic_ words inside of it.
+
+    - This is the first list item in a list block
+    - This is a list item
+    - This is another list item
+                             '''))
 
 main()
