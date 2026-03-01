49740dec3e91a06ec7ca660e331e3fa73f6f56fd
hectoribarraiwp2024-eng <hector.ibarra.iwp2024@gmail.com>
2026-03-01 12:58:16 -0600

    12: created a static directory in the root of the project, then made a couple recursive functions to delete any files/directories in the public directory, and copy any files/directories from static to public directories. i put these functions in the main.py file, then i added public/ to my .gitignore file

--- src/main.py
+++ src/main.py
@@ -1,8 +1,35 @@
-from markdown_to_htmlnode import markdown_to_html_node
+import os
+import shutil
 
 def main():
-    pass
+    CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+    PROJECT_ROOT = os.path.dirname(CURRENT_DIR)
+
+    public_path = os.path.join(PROJECT_ROOT, "public")
+    static_path = os.path.join(PROJECT_ROOT, "static")
+
+    clear_directory(public_path)
+    copy_directory(static_path , public_path)
     
-#"<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>"
+def clear_directory(path):
+    for item in os.listdir(path):
+        full_path = os.path.join(path, item)
+
+        if os.path.isfile(full_path):
+            os.remove(full_path)
+        elif os.path.isdir(full_path):
+            clear_directory(full_path)
+            os.rmdir(full_path)
+
+def copy_directory(src, dest):
+    for item in os.listdir(src):
+        src_path = os.path.join(src, item)
+        dest_path = os.path.join(dest, item)
+
+        if os.path.isfile(src_path):
+            shutil.copy(src_path, dest_path)
+        elif os.path.isdir(src_path):
+            os.makedirs(dest_path, exist_ok=True)
+            copy_directory(src_path, dest_path)
 
 main()
